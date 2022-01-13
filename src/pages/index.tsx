d800e6c5edaefab6f292c38ff830cfd855322300
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-13 17:04:21 -0300

    Adicionado static site generation - SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   //  Pegar info dos produtos
   // const price = await stripe.prices.retrieve('price_1KHXPlBr5eSeDVykMzC5ZdE5', {
   //   expand: ['product']
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return { 
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }

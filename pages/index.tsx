4c7ac6861a2e218738f1e9643b9b08cdb8b97754
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-13 10:54:05 -0300

    Adicionado estilos globais

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello Next</h1>
+      <h1>Hello Next</h1>
     </>
   )
 }

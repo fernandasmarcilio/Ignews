36314e7865d0894008e504ec851af6d9b270d65b
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-13 10:42:46 -0300

    Adicionado o arquivo _document e title na Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello Next</h1>
     </>
   )

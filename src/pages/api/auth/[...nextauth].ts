8566ab3d4ea9755cfd4cbefc47517e01eed53d5c
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-14 20:26:18 -0300

    Adicionado e configurado FaunaDB; Armazena o login do usuário no DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,42 @@ export default NextAuth({
       }
     }),
   ],
+  secret: process.env.SIGNING_KEY,
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        
+        return false;        
+      }
+      
+    }
+  }
 })

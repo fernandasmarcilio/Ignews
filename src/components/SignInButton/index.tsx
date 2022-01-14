cf60f849b0a12c5c04cb36b94e1471dde44eeedd
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-01-14 11:23:21 -0300

    Adicionado login e logout com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './signin-button.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession(); 
   
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.container}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04D361" />
-      Fernanda Marcilio
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button 
       type="button"
-      className={styles.signInButton}
+      className={styles.container}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sign in with Github

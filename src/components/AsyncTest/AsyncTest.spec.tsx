a8f14e7cec606ef91c6b65778619c2882aa8439a
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-17 11:01:37 -0300

    Adiciona anotações dos tipos de teste

--- src/components/AsyncTest/AsyncTest.spec.tsx
+++ src/components/AsyncTest/AsyncTest.spec.tsx
@@ -9,15 +9,19 @@ describe("AsyncTest component", () => {
 
     screen.logTestingPlaygroundURL()
 
+    // screen.get -> procura elemento de forma sícrona, se não encontrar da erro
+    // screen.query-> procura elemento de forma sícrona, se não encontrar não vai dar erro
+    // screen.find-> procura elemento, se não encontrar vai ficar observando/monitando por um tempo. Se não for encontrado vai dar erro.
+
     expect(screen.getByText("Hello AsynTest Component")).toBeInTheDocument();
     // expect(await screen.findByText("Button")).toBeInTheDocument();
     await waitFor(() => {
       return expect(screen.getByText("Button")).toBeInTheDocument();
     })
 
-    // await waitFor(() => {
-    //   return expect(screen.queryByText('Button2')).not.toBeInTheDocument();
-    // })
+    await waitFor(() => {
+      return expect(screen.queryByText('Button2')).not.toBeInTheDocument();
+    })
     // await waitForElementToBeRemoved(screen.queryByText('Button2'))
   });
 
